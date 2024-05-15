e38b944b622dfb36a7120acf9f2a3c5180c464c2
rsgamertv <Wersuser2@gmail.com>
2024-05-15 13:38:48 +0300

    commit

--- lib/authorizationPage/screen/authorizaiton_screen.dart
+++ lib/authorizationPage/screen/authorizaiton_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rent_car_app/helper/adaptive_helper.dart';
 import 'package:rent_car_app/main.dart';
+import 'package:sizer/sizer.dart';
 
 class AuthorizaitonScreen extends StatelessWidget {
   const AuthorizaitonScreen({super.key});
@@ -8,10 +9,12 @@ class AuthorizaitonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
         body: Container(
+          decoration: BoxDecoration(image:  DecorationImage(image: AssetImage('assets/png/background.jpeg'),fit: BoxFit.cover)),
           height: height(context),
           width: width(context),
+          padding: EdgeInsets.only(left: 5.w, right: 5.w,top: 15.h),
+          child: Column(),
         ),
     );
   }
