84402b2de1bdfd5ddf81de060ee01710ea56f2d7
rsgamertv <Wersuser2@gmail.com>
2024-05-15 15:39:50 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
-import 'package:rent_car_app/authorizationPage/screen/authorizaiton_screen.dart';
 import 'package:rent_car_app/core/routes.dart';
+import 'package:rent_car_app/firebase_options.dart';
 import 'package:rent_car_app/theme/theme.dart';
 import 'package:sizer/sizer.dart';
 import 'package:talker_bloc_logger/talker_bloc_logger_observer.dart';
@@ -11,7 +12,11 @@ import 'package:talker_bloc_logger/talker_bloc_logger_settings.dart';
 import 'package:talker_dio_logger/talker_dio_logger_interceptor.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   initDependencies();
   runApp(const MyApp());
 }
