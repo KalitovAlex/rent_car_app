5a662283d229a7c28c12bc1cdd9247a05e622ebb
rsgamertv <Wersuser2@gmail.com>
2024-05-21 09:27:44 +0300

    commit

--- lib/authorizationPage/repository/user_repository.dart
+++ lib/authorizationPage/repository/user_repository.dart
@@ -1,5 +1,19 @@
-import 'package:rent_car_app/authorizationPage/model/userModel/user.dart';
+import 'package:dio/dio.dart';
+import 'package:rent_car_app/core/globals.dart';
 
 class UserRepository {
-  final user = const User();
+  final user = gia;
+  Future<bool> authorization() async{
+    try{
+      final response = await Dio().get(
+      'http://$ip/api/users?email=${gia.email}&password=${gia.password}'
+      );
+      final responseData = response.data as Map<String,dynamic>;
+      gia = gia.copyWith(name: responseData['name'] , phoneNumber: responseData['phone_number'], username: responseData['username']);
+      return true;
+    } catch (e){
+      talker.error(e);
+      return false;
+    }
+  }
 }
