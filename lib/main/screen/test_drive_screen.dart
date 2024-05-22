d2889398315b273eb43284358111f05ec34e8ae3
rsgamertv <Wersuser2@gmail.com>
2024-05-22 10:58:10 +0300

    commit

--- lib/main/screen/test_drive_screen.dart
+++ lib/main/screen/test_drive_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:rent_car_app/main/widget/test_drive_screen_listview.dart';
 import 'package:rent_car_app/theme/colors.dart';
+import 'package:sizer/sizer.dart';
 
 class TestDriveScreen extends StatelessWidget {
   const TestDriveScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: backgroundColor,
+      body: Container(
+        padding: EdgeInsets.only(left: 5.w,right: 5.w,top: 5.h),
+        child: Column(
+          children: [
+          ListView.builder(
+            itemCount: 1,
+            itemBuilder: (BuildContext context, int index) {
+              return testDriveScreenListView();
+            },
+          ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/main/widget/test_drive_screen_listview.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Widget testDriveScreenListView(){
+  return Column(
+    children: [
+      Image.network('https://s3-alpha-sig.figma.com/img/72f6/89ea/c7bc0f00ed2cc903b41a96f8e04cbdf5?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VVriV8-lBakTJffn5IzhsCDy-Hc2tMRNuacvb4idV1OTV6N-DKKujCgzq0GyqAsX1pd3-g-3vO8d8MhJVQIbKzQnbNtiy9OHRzAMaouuyiXr9gSWzUbS1IfD0qe1r8KbCXKyopqYPQcpgO0PVKvxGAJ38dgHRbQfJs8-4Vn78SXVYby-4VVplGgngc~q6M1OiXkFXMLK-UCvhhInf0gGxhbOOcey62aQrLcRDn9uZb-MjPHkuXF41R-9Rj5aJm0lo509PCMHTyvW33rQ71U3UFEivD2eQ2u6-INlbKaumJ35MynVjFcaAWHoj~SfoTVi4Rnv1tKG8cWDBtp93mrudw__'),
+
+    ],
+  );
+}
