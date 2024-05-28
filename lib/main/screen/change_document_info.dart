5f4dfa34e7f53c328b0cb711790be61f55969fb5
rsgamertv <Wersuser2@gmail.com>
2024-05-28 22:20:20 +0300

    commit

--- lib/main/screen/change_document_info.dart
+++ lib/main/screen/change_document_info.dart
@@ -1,7 +1,8 @@
+import 'dart:js_interop';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:rent_car_app/core/globals.dart';
-import 'package:rent_car_app/main/widget/car_date_decoration.dart';
 import 'package:sizer/sizer.dart';
 
 import '../../helper/adaptive_helper.dart';
@@ -17,7 +18,9 @@ class ChangeDocumentInfo extends StatefulWidget {
 }
 
 class _ChangeDocumentInfoState extends State<ChangeDocumentInfo> {
-    TextEditingController dateController = TextEditingController();
+    TextEditingController dateBirthController = TextEditingController();
+    TextEditingController dateGiveController = TextEditingController();
+    TextEditingController dateExpiredController = TextEditingController();
     TextEditingController nameController = TextEditingController();
     TextEditingController whoGiveController = TextEditingController();
     TextEditingController documentNumberController = TextEditingController();
@@ -32,10 +35,32 @@ class _ChangeDocumentInfoState extends State<ChangeDocumentInfo> {
       whenGiveController.text = documentModel.whereIssued!;
       categoriesController.text = documentModel.categories!;
     }
+    @override
+    void dispose() {
+      dateBirthController.dispose();
+      dateBirthController.dispose();
+      dateExpiredController.dispose();
+      nameController.dispose();
+      whoGiveController.dispose();
+      documentNumberController.dispose();
+      whenGiveController.dispose();
+      categoriesController.dispose();
+      super.dispose();
+    }
   @override
-  Widget build(BuildContext context) {
-   void selectDate() {
-    showDatePicker(context: context, firstDate: DateTime(1950), lastDate: DateTime(2024));
+  Widget build(BuildContext context,) {
+   void selectDate(TextEditingController controller) {showDatePicker(context: context, firstDate: DateTime(1950), lastDate: DateTime(2050)).then((value) {
+    setState(() {
+      controller.text = "${value!.year}-${value.month}-${value.day}";
+    });
+   });
+  }
+     void selectBirthDate(TextEditingController controller) {
+    showDatePicker(context: context, firstDate: DateTime(1950), lastDate: DateTime(2020)).then((value){
+      setState(() {
+        controller.text = "${value!.year}-${value.month}-${value.day}";
+      });
+    });
   }
     return Scaffold(
           appBar: AppBar(
@@ -59,12 +84,34 @@ class _ChangeDocumentInfoState extends State<ChangeDocumentInfo> {
                     carTextDecoration('ФИО', nameController, context),
                         Text('Дата рождения',style: Theme.of(context).textTheme.titleSmall,),
                         SizedBox(height: 1.h,),
-                        Container(decoration: info_container_decoration,child: TextFormField(decoration: invisible_input_decoration('укажите дату рождения'), controller: dateController,readOnly: true, onTap: () => selectDate())),
+                        Container(decoration: info_container_decoration,child: TextFormField(decoration: invisible_input_decoration('укажите дату рождения'), controller: dateBirthController,readOnly: true, onTap: () => selectBirthDate(dateBirthController))),
+                        SizedBox(height: 1.h,),
+                        Text('Дата выдачи',style: Theme.of(context).textTheme.titleSmall,),
+                        SizedBox(height: 1.h,),
+                        Container(decoration: info_container_decoration,child: TextFormField(decoration: invisible_input_decoration('укажите дату выдачи'), controller: dateGiveController,readOnly: true, onTap: () => selectDate(dateGiveController))),
+                        SizedBox(height: 1.h,),
+                        Text('Укажите дату окончания',style: Theme.of(context).textTheme.titleSmall,),
+                        SizedBox(height: 1.h,),
+                        Container(decoration: info_container_decoration,child: TextFormField(decoration: invisible_input_decoration('укажите дату окончания'), controller: dateExpiredController,readOnly: true, onTap: () => selectDate(dateExpiredController))),
                         SizedBox(height: 1.h,),
                     carTextDecoration('Кем выдан', whoGiveController, context),
                     carTextDecoration('Номер', documentNumberController, context),
                     carTextDecoration('Где выдан', whenGiveController, context),
                     carTextDecoration('Категория', categoriesController, context),
+                    SizedBox(height: 2.h,),
+                    Center(
+                      heightFactor: BorderSide.strokeAlignOutside,
+                      child: Container(
+                        width: 80.w,
+                        height: 6.h,
+                        decoration: button_black_decoration,
+                        child: TextButton(onPressed: () {
+                         if(nameController.text != null && nameController.text != userModel.name && dateBirthController.text.toString() != documentModel.birthDate.toString() && dateBirthController.text != null && dateGiveController.text.toString() != documentModel.dateOfIssue && dateGiveController.text != null && dateExpiredController.text.toString() != documentModel.endDate.toString() && dateExpiredController.text != null && whoGiveController.text != documentModel.issuedBy && whenGiveController.text != '' && documentNumberController.text != documentModel.number && documentNumberController.text != '' && whenGiveController.text != documentModel.whereIssued && whenGiveController.text != '' && categoriesController.text != documentModel.toExternalReference && categoriesController.text != ''){
+                          
+                         }
+                        }, child: Text('Изменить', style: Theme.of(context).textTheme.labelMedium,)),
+                      ),
+                    )
                   ],
                 ),
               ),
