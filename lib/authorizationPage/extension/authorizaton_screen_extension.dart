52f7ff1e3cc5456c2b5f52ce242007df2eaa0c3c
rsgamertv <Wersuser2@gmail.com>
2024-05-28 12:17:31 +0300

    commit

--- lib/authorizationPage/extension/authorizaton_screen_extension.dart
+++ lib/authorizationPage/extension/authorizaton_screen_extension.dart
@@ -1,6 +1,9 @@
 part of '../screen/authorizaiton_screen.dart';
 extension on _AuthorizaitonScreenState{
   void authorization () async{
-    userModel = userModel.copyWith(email: emailController.text, password: passwordController.text);
+    userModel = userModel.copyWith(
+      email: emailController.text, 
+      password: passwordController.text
+    );
   }
 }
