bca7f9401db070b0b662e9a8fd192726550ebc36
rsgamertv <Wersuser2@gmail.com>
2024-05-30 10:18:40 +0300

    commit

--- lib/main/extensions/change_document_info_extensions.dart
+++ lib/main/extensions/change_document_info_extensions.dart
@@ -1,9 +1,12 @@
 part of '../screen/change_document_info.dart';
 
 extension on _ChangeDocumentInfoState{
-  // void changeDocument(){
-  //   documentModel = documentModel.copyWith(
-  //     name: nameController.text,
-  //     birthDate: dateBirthController.text.toString());
-  // }
+  void changeDocument(){
+    documentModel = documentModel.copyWith(
+      name: nameController.text,
+      birthDate:DateTime.parse(dateBirthController.text.toString()),
+      
+      
+      );
+  }
 }
